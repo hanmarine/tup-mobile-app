4cd413a7ea692da00c17a2520ac50a0034e0eac3
hanmarine <hanajoy.marinduque@tup.edu.ph>
2024-06-15 22:52:03 +0800

    Added navigation pane

--- app/src/main/java/com/groupfour/tupmobileapp/MainActivity.kt
+++ app/src/main/java/com/groupfour/tupmobileapp/MainActivity.kt
@@ -5,8 +5,6 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
 import android.widget.Toast
 import com.google.firebase.database.DatabaseReference
@@ -42,12 +40,6 @@ class MainActivity : AppCompatActivity() {
             } else {
                 readData(studentID, password)
             }
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 
--- app/src/main/java/com/groupfour/tupmobileapp/ScheduleActivity.kt
+++ app/src/main/java/com/groupfour/tupmobileapp/ScheduleActivity.kt
@@ -1,20 +1,57 @@
 package com.groupfour.tupmobileapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class ScheduleActivity : AppCompatActivity() {
+
+    // Navigation Pane
+    private lateinit var toggle : ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_schedule)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            when(it.itemId){
+                R.id.nav_schedule -> {
+                    val scheduleIntent = Intent(this, ScheduleActivity::class.java)
+                    startActivity(scheduleIntent)
+                }
+                R.id.nav_message -> {
+                    val messageIntent = Intent(this, MessageActivity::class.java)
+                    startActivity(messageIntent)
+                }
+                R.id.nav_logout -> {
+                    val logoutIntent = Intent(this, MainActivity::class.java)
+                    startActivity(logoutIntent)
+                }
+            }
+            true
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
